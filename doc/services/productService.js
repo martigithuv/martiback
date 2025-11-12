4f10cce04c74a4412e8443f568cfb1886fbd89f7
martigithuv <mamemu@jviladoms.cat>
2025-11-12 12:15:23 +0100

    Delete doc/services directory

--- doc/services/productService.js
+++
@@ -1,8 +0,0 @@
-const Product = require('../models/product.js'); 
-const createProduct = async (productData) => { 
-    const newProduct = new Product(productData); 
-    return await newProduct.save(); 
-}; 
-module.exports = { 
-    createProduct, 
-}; 
